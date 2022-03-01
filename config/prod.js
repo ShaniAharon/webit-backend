7005fc4322f1aac6648e404b80a1ca4e146cb240
ShaniAharon <48124069+ShaniAharon@users.noreply.github.com>
2022-03-01 20:28:55 +0200

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
   // dbURL: 'mongodb://localhost:27017',
   dbURL:
-    'mongodb+srv://Yarden:72lc5o7Xkqnm6KNl@cluster0.lsmqg.mongodb.net/toy_db?retryWrites=true&w=majority',
+    '',
 }
