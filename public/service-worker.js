76a3b1e09ccce13e509632b72a668a1de4677c85
Yarden Mathieson <yardenm2710@gmail.com>
2022-08-16 16:11:14 +0300

    minor fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,28 +11,22 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
 
-importScripts(
-<<<<<<< HEAD
-  "/precache-manifest.547890d5310fcd60c18dfa9c30816a64.js"
-=======
-  "/precache-manifest.cecda4e94564a2da3256bec2372d203e.js"
->>>>>>> 6048da8829320e60ec815d4d859f79e0af013d61
-);
+importScripts('/precache-manifest.9935ef1be548b0011ab5c16fb4d5fc1f.js')
 
-workbox.core.setCacheNameDetails({prefix: "webit"});
+workbox.core.setCacheNameDetails({ prefix: 'webit' })
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
+    self.skipWaiting()
   }
-});
+})
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
